31b917b6f9fae8c8a223b3ad55aba0942b2fd4a6
AlejandroJesusSerrano <alejandrojserrano@hotmail.com>
2024-03-15 13:14:36 -0300

    navbar advances

--- smarthunt_front/src/components/NavBar/NavBar.js
+++ smarthunt_front/src/components/NavBar/NavBar.js
@@ -2,39 +2,58 @@
 
 export const NavBar = () => {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+    <nav className="navbar nav-underline navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid p-0">
+        <a className="navbar-brand p-0 m-0" href="#">
+          <img src="./logo.png" alt="SMART HUNT LOGO" width="166" height="45.03" className="ms-3"/>
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mx-auto mx-5 mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active text-primary" aria-current="page" href="#">Inicio</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Consultas</a>
             </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Cargar Equipamiento
               </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider"/></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Computadoras</a></li>
+                <li><a className="dropdown-item" href="#">Monitores</a></li>
+                <li><a className="dropdown-item" href="#">Impresoras & Scanners</a></li>
+                <li><a className="dropdown-item" href="#">Perifericos</a></li>
+                <li><hr className="dropdown-divider"/></li>
+                <li><a className="dropdown-item" href="#">Dependecias</a></li>
+                <li><a className="dropdown-item" href="#">Oficinas</a></li>
+                <li><a className="dropdown-item" href="#">Empleados</a></li>
               </ul>
             </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Cargas Base
+              </a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Marcas</a></li>
+                <li><a className="dropdown-item" href="#">Tipos de Dispositivo</a></li>
+                <li><a className="dropdown-item" href="#">Modelos</a></li>
+                <li><hr className="dropdown-divider"/></li>
+                <li><a className="dropdown-item" href="#">Tecnicos</a></li>
+                <li><hr className="dropdown-divider"/></li>
+                <li><a className="dropdown-item" href="#">Provincias</a></li>
+                <li><a className="dropdown-item" href="#">Localidades</a></li>
+                <li><a className="dropdown-item" href="#">Edificios</a></li>
+              </ul>
             </li>
           </ul>
-          <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
+          {/* <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form> */}
         </div>
       </div>
     </nav>

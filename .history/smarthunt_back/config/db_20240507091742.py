6c9ff9c7b2cc6dfe87bdecbee4ccd19f2ec52cb8
AlejandroJesusSerrano <alejandrojserrano@hotmail.com>
2024-05-10 10:38:13 -0300

    "finish counter"

---
+++ .history/smarthunt_back/config/db_20240507091742.py
@@ -0,0 +1,10 @@
+POSTGRESQL = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'smartHunt2',
+    'USER': 'postgres',
+    'PASSWORD': 'tecnico',
+    'PORT': '5432'
+  }
+
+}
---
+++ .history/smarthunt_back/config/db_20240507091750.py
@@ -0,0 +1,10 @@
+POSTGRESQL = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'smartHunt2',
+    'USER': 'postgres',
+    'PASSWORD': 'Am0rc170',
+    'PORT': '5432'
+  }
+
+}
